06f2a4580e495282e93d6654ef04f463038a334d
Gauravsingh2841 <gauravzapak12@gmail.com>
2025-12-15 17:11:00 +0530

    This is my second commit for this repository.

---
+++ src/ex_01_Java_Basics/TestDemo1.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class TestDemo1 {
+    public static void main(String[] args){
+        System.out.println(12+8);
+        System.out.println("This is the second commit");
+    }
+}
