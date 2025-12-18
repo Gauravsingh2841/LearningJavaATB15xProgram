e3e969a629e0b7ed10666e95f250f7ef4d04f3ef
Gauravsingh2841 <gauravzapak12@gmail.com>
2025-12-18 10:50:09 +0530

    This is my third commit.

---
+++ src/ex_02_Java_Basics/Task001_Table_of_9_Another_Approach.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Task001_Table_of_9_Another_Approach {
+    public static void main(String[] args){
+        int num = 9;
+        for(int i = 1; i<=10; i++)
+        {
+            System.out.printf("%d * 1 = %d%n",num,num*i);
+        }
+    }
+}
