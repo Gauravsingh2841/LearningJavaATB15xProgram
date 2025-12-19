c39be4fdaa14d63837bb08940293acd474a908e0
Gauravsingh2841 <gauravzapak12@gmail.com>
2025-12-19 12:07:59 +0530

    19th Dec 2025 Commit.

---
+++ src/ex_02_Variables_DataTypes/Task001_Table_of_9_Another_Approach.java
@@ -0,0 +1,11 @@
+package ex_02_Variables_DataTypes;
+
+public class Task001_Table_of_9_Another_Approach {
+    public static void main(String[] args){
+        int num = 9;
+        for(int i = 1; i<=10; i++)
+        {
+            System.out.printf("%d * %d = %d%n",num,i,num*i);
+        }
+    }
+}
