f12bf638de514263525c9facae7c0d03b8318460
Gauravsingh2841 <gauravzapak12@gmail.com>
2026-02-10 16:20:09 +0530

    OOPs Coding Challenge 4 Commit.

---
+++ src/task/OOPs_Test/CodingChallenge4.java
@@ -0,0 +1,48 @@
+package task.OOPs_Test;
+
+public class CodingChallenge4 {
+    public static void main(String[] args){
+        BankAccount bc = new BankAccount(-300);
+
+        bc.deposit(500);
+        System.out.println("Balance after deposit: " + bc.getBalance());
+
+        bc.withdraw(200);
+        System.out.println("Balance after withdrawal: " + bc.getBalance());
+
+    }
+}
+
+class BankAccount{
+    private double balance;
+
+    public BankAccount(double initialBalance){
+        if(initialBalance<0){
+            System.out.println("Initial balance cannot be negative. Setting balance to 0.");
+            this.balance=0;
+        }else{
+            this.balance=initialBalance;
+        }
+    }
+
+    public void deposit(double depositAmount){
+        if(depositAmount>0){
+            balance += depositAmount;
+        }else{
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double withdrawalAmount){
+        if(withdrawalAmount>0 && withdrawalAmount<=balance){
+            balance -= withdrawalAmount;
+        }else{
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+}
