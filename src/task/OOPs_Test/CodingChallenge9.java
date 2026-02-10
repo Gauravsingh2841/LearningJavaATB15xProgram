498707e94de4a87782e180802876daa208488310
Gauravsingh2841 <gauravzapak12@gmail.com>
2026-02-10 18:27:33 +0530

    OOPs Coding Challenge 9 Commit.

---
+++ src/task/OOPs_Test/CodingChallenge9.java
@@ -0,0 +1,27 @@
+package task.OOPs_Test;
+
+public class CodingChallenge9 {
+    public static void main(String[] args){
+        Drawable circle = new Circle1();
+        Drawable rectangle = new Rectangle1();
+
+        circle.draw();
+        rectangle.draw();
+    }
+}
+
+interface Drawable{
+    public abstract void draw();
+}
+
+class Circle1 implements Drawable{
+    public void draw(){
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Rectangle1 implements Drawable{
+    public void draw(){
+        System.out.println("Drawing a Rectangle");
+    }
+}
