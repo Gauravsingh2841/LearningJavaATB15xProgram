113c8b48bc5b144a343eccfd445893b12bacd788
Gauravsingh2841 <gauravzapak12@gmail.com>
2026-02-15 17:29:42 +0530

    9th Feb 2026 Commit.

---
+++ src/ex_Exceptions/CF_01_LIST/LabConvertArrayToList.java
@@ -0,0 +1,12 @@
+package ex_Exceptions.CF_01_LIST;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LabConvertArrayToList {
+    public static void main(String[] args) {
+        String arr[] = {"Java","Python","C#"};
+        List<String> list = Arrays.asList(arr);
+        System.out.println(list);
+    }
+}
---
+++ src/ex_Exceptions/CF_01_LIST/LabExample.java
@@ -0,0 +1,35 @@
+package ex_Exceptions.CF_01_LIST;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LabExample {
+    public static void main(String[] args) {
+        // Requirement where we want to modify the collection
+        // many times, then we always use the linkedlist.
+        Scanner scanner = new Scanner(System.in);
+        String continueInput = "Y";
+        List<String> names = new ArrayList<>();
+
+        while(continueInput.equalsIgnoreCase("Y")){
+
+            System.out.println("Enter the name that you want to store");
+            String name = scanner.next();
+            names.add(name);
+            scanner.nextLine();
+            System.out.println("Do you want to add another one? Y/N");
+            continueInput = scanner.nextLine();
+
+        }
+
+        for (String name: names){
+            System.out.println(name);
+        }
+
+        scanner.close();
+        //LinkedList l = new LinkedList();
+
+
+    }
+}
