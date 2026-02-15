aa06250ff85d33284218dcead5c3306dacde8cb3
Gauravsingh2841 <gauravzapak12@gmail.com>
2026-02-15 17:42:25 +0530

    11th Feb 2026 Commit.

---
+++ src/ex_30_Collection_Framework/CF_02_SET/Lab242_HS.java
@@ -0,0 +1,26 @@
+package ex_30_Collection_Framework.CF_02_SET;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Lab242_HS {
+    public static void main(String[] args) {
+
+
+        Set<Integer> set = new HashSet();
+        set.add(9);
+        set.add(3);
+        set.add(1);
+        System.out.println("Set elements: " + set);
+
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+
+
+    }
+}
